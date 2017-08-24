e9377403480b8143349e940ef2ccaaa93e3ba59f
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-08-24 10:28:02 -0600

    Complete Level 2

--- elevator.js
+++ elevator.js
@@ -1,5 +1,35 @@
 export default class Elevator {
   constructor() {
+    this.currentFloor = 0;
+    this.requests = 'tbd';
+    this.riders = [];
+    this.stopCount = 0;
+    this.floorsTraversed = 0;
+  }
+  
+  reset() {
+    this.floor = 0;
+    this.currentFloor = 0;
+    this.requests = 'tbd';
+    this.riders = [];
+    this.stopCount = 0;
+    this.floorsTraversed = 0;
+  }
+  
+  goToFloor(user) {
+    this.floorsTraversed = this.floorsTraversed + Math.abs(user.currentFloor - this.currentFloor);
+    this.currentFloor = user.currentFloor;
+    this.stopCount++;
+    console.log('Pick up:', this.currentFloor);
     
+    this.riders.push(user);
+    console.log('Riders:', this.riders);
+    
+    this.floorsTraversed = this.floorsTraversed + Math.abs(user.dropOffFloor - this.currentFloor);
+    this.currentFloor = user.dropOffFloor;
+    this.stopCount++;
+    console.log('Drop off:', this.currentFloor);
   }
 }
+
+
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -12,11 +12,43 @@ describe('Elevator', function() {
     elevator.reset();
   });
 
+  it('should have initial properties', () => {
+    elevator.currentFloor = 0;
+    elevator.requests = 'tbd';
+    elevator.riders = [];
+    elevator.stopCount = 0;
+    elevator.floorsTraversed = 0;
+  });
+
+  it('should reset the elevator', () => {
+    elevator.currentFloor = 10;
+    elevator.requests = 'hi';
+    elevator.riders = ['bob'];
+    elevator.stopCount = 10;
+    elevator.floorsTraversed = 10;
+    
+    assert.equal(elevator.currentFloor, 10);
+    assert.equal(elevator.requests, 'hi');
+    assert.deepEqual(elevator.riders, ['bob']);
+    assert.equal(elevator.stopCount, 10);
+    assert.equal(elevator.floorsTraversed, 10);
+
+    elevator.reset();
+
+    assert.equal(elevator.currentFloor, 0);
+    assert.equal(elevator.requests, 'tbd');
+    assert.deepEqual(elevator.riders, []);
+    assert.equal(elevator.stopCount, 0);
+    assert.equal(elevator.floorsTraversed, 0);
+  });
+
   it('should bring a rider to a floor above their current floor', () => {
     let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
     elevator.goToFloor(mockUser);
 
     assert.equal(elevator.currentFloor, 5);
+    assert.equal(elevator.stopCount, 2);
+    assert.equal(elevator.floorsTraversed, 5);
   });
 
   it('should bring a rider to a floor below their current floor', () => {
@@ -24,5 +56,7 @@ describe('Elevator', function() {
     elevator.goToFloor(mockUser);
 
     assert.equal(elevator.currentFloor, 3);
+    assert.equal(elevator.stopCount, 2);
+    assert.equal(elevator.floorsTraversed, 13);
   });
 });
